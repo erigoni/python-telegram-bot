9e766ca6cc031f02a1feb86622ec8f0df90a8969
Esteban <estebanrigoni@hotmail.com>
2022-07-31 15:05:46 -0300

    Update bot.py
    Change env variable name

--- bot.py
+++ bot.py
@@ -193,7 +193,7 @@ def error(update, context):
 
 def main():
     # Create the Updater and pass it your bot's token.
-    telegram_token = os.environ['TELEGRAM_TOKEN']
+    telegram_token = os.environ.get("TOKEN")
     updater = Updater(telegram_token, use_context=True)
 
     # Get the dispatcher to register handlers
